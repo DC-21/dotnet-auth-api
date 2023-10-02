395fd5de59aa1080cccc69c3a81c7dc80bf72c65
DC-21 <cholahkuboko@gmail.com>
2023-10-02 23:06:21 +0200

    first commit

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -14,50 +14,98 @@ namespace AuthApi.Controllers
 {
     [ApiController]
     [Route("api/v1/authenticate")]
-    public class AuthenticationController:ControllerBase{
+    public class AuthenticationController : ControllerBase
+    {
         private readonly UserManager<User> _userManager;
-        public AuthenticationController(UserManager<User>userManager){
+        public AuthenticationController(UserManager<User> userManager)
+        {
             _userManager = userManager;
         }
         [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var result = await RegisterAsync(request);
+            return result.Success ? Ok(result) : BadRequest(result.Message);
+        }
+        private async Task<RegisterResponse> RegisterAsync(RegisterRequest request)
+        {
+            try
+            {
+                var userExists = await _userManager.FindByEmailAsync(request.Email);
+                if (userExists != null)
+                    return new RegisterResponse { Message = "User already exists", Success = false };
+
+                userExists = new User
+                {
+                    FullName = request.FullName,
+                    Email = request.Email,
+                    ConcurrencyStamp = Guid.NewGuid().ToString(),
+                    UserName = request.UserName,
+                };
+                var createUserResult = await _userManager.CreateAsync(userExists, request.Password);
+                if (!createUserResult.Succeeded) return new RegisterResponse { Message = $"create user failed{createUserResult?.Errors?.First()?.Description}", Success = false };
+                var addUserToRoleResult = await _userManager.AddToRoleAsync(userExists, "USER");
+                if (!addUserToRoleResult.Succeeded)
+                    return new RegisterResponse { Message = $"create user succeded but could not add to role {addUserToRoleResult?.Errors?.First()?.Description}", Success = false };
+
+            }
+            catch (Exception ex)
+            {
+                return new RegisterResponse { Message = ex.Message, Success = false };
+            }
+        }
+
+        [HttpPost]
         [Route("login")]
-        [ProducesResponseType((int)HttpStatusCode.OK,Type=typeof(LoginResponse))]
-        public async Task<IActionResult>Login([FromBody]LoginRequest request){
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(LoginResponse))]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
             var result = await LoginAsync(request);
             return result.Success ? Ok(result) : BadRequest(result.Message);
         }
-        private async Task<LoginResponse>LoginAsync(LoginRequest request){
-            var user = await _userManager.FindByEmailAsync(request.Email);
-            if(user is null)return new LoginResponse{Message="Invalid email/password",Success=false};
+        private async Task<LoginResponse> LoginAsync(LoginRequest request)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(request.Email);
+                if (user is null) return new LoginResponse { Message = "Invalid email/password", Success = false };
 
-            var claims = new List<Claim>{
+                var claims = new List<Claim>{
                 new Claim(JwtRegisteredClaimNames.Sub,user.Id.ToString()),
                 new Claim(ClaimTypes.Name,user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
                 new Claim(ClaimTypes.NameIdentifier,user.Id.ToString())
-            };
-            var roles = await _userManager.GetRolesAsync(user);
-            var roleClaims = roles.Select(x=>new Claim(ClaimTypes.Role,x));
-            claims.AddRange(roleClaims);
+                 };
+                var roles = await _userManager.GetRolesAsync(user);
+                var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x));
+                claims.AddRange(roleClaims);
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("lswek3u4uo2u4"));
-            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
-            var expires =DateTime.Now.AddMinutes(30);
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("lswek3u4uo2u4"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var expires = DateTime.Now.AddMinutes(30);
 
-            var token = new JwtSecurityToken(
-                issuer:"https://localhost:5001",
-                audience:"https://localhost:5001",
-                claims:claims,
-                expires:expires,
-                signingCredentials:creds
-            );
-            return new LoginResponse{
-                AccessToken=new JwtSecurityTokenHandler().WriteToken(token),
-                Message="Login Successful",
-                Email=user?.Email,
-                Success=true,
-                UserId=user?.Id.ToString()
-            };
+                var token = new JwtSecurityToken(
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
+                    claims: claims,
+                    expires: expires,
+                    signingCredentials: creds
+                );
+                return new LoginResponse
+                {
+                    AccessToken = new JwtSecurityTokenHandler().WriteToken(token),
+                    Message = "Login Successful",
+                    Email = user?.Email,
+                    Success = true,
+                    UserId = user?.Id.ToString()
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new LoginResponse { Success = false, Message = ex.Message };
+            }
         }
     }
 }
